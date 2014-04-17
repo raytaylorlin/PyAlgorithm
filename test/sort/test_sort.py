ffb830ed376916239faba1f3d3a312062704d48c
RayTaylorLin <raytaylorlin@gmail.com>
2014-04-17 22:15:05 +0800

    将排序算法文件测试样例的读取抽象到test_sort中

--- test/sort/test_sort.py
+++ test/sort/test_sort.py
@@ -3,12 +3,27 @@
 
 import random
 import unittest
-from test.util import getCases
+import test.util as util
 
 
 TEST_CASE_DIR = 'data'
 
+
 class TestSorting(unittest.TestCase):
+
     def setUp(self):
-        self.cases = getCases(TEST_CASE_DIR)
-        # print self.cases
+        self.cases = util.getCases(TEST_CASE_DIR)
+
+    def getStandardData(self, dataFile):
+        """读取文件内容，并将数据转换为标准形式
+        Args:
+            dataFile: 存放数据的文件（在排序测试中，文件应该是每行1个整数）
+        Return:
+            一个整数列表
+        """
+        with open(dataFile) as file:
+            data = file.read()
+        return self.__standardize(data)
+
+    def __standardize(self, data):
+        return list(map(int, data.split('\n')))
