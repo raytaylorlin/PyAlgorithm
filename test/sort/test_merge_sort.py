8898af3aa682324d273130dd5505d628a1735ca3
RayTaylorLin <raytaylorlin@gmail.com>
2014-04-25 10:40:28 +0800

    重构公共排序类，添加测试正常情况的公共方法

--- test/sort/test_merge_sort.py
+++ test/sort/test_merge_sort.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import unittest
 from algorithm.sort.merge_sort import mergeSort
 from test_sort import *
 
@@ -9,16 +8,7 @@ from test_sort import *
 class TestMergeSort(TestSorting):
 
     def testBaseCase(self):
-        u"""测试正常情况（随机产生10个数）"""
-
-        self.input = range(10)
-        random.shuffle(self.input)
-        print 'Sorting unit test data: ', self.input
-        self.correct = range(10)
-
-        self.output = mergeSort(self.input)
-        print 'Result: ', self.output
-        self.assertEqual(self.correct, self.output)
+        self._testBaseCase(mergeSort)
 
     def testMergeSortCase(self):
         u"""测试文件样例"""
--- test/sort/test_sort.py
+++ test/sort/test_sort.py
@@ -9,10 +9,25 @@ logger = util.getLogger('test.sort')
 
 
 class TestSorting(util.PyAlgorithmTestCase):
+    TEST_CASE_FORMAT = '''
+    Input: list = %s
+    Result: %s
+    '''
 
     def setUp(self):
         super(TestSorting, self).setUp(logger)
         self.cases = util.getInputAnswerCases('data', 'sort')
+
+    def _testBaseCase(self, sortFunc):
+        u"""测试正常情况（随机产生10个数）"""
+
+        self.input = range(10)
+        random.shuffle(self.input)
+        self.correct = range(10)
+        self.output = sortFunc(self.input)
+
+        logger.debug(TestSorting.TEST_CASE_FORMAT, self.input, self.output)
+        self.assertEqual(self.correct, self.output)
 
     def _testSortingCase(self, sortFunc):
         def standardizeInput(content):
