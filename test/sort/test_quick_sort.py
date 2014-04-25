06301e30903401187ea2a8b907a8a9478021e5ff
RayTaylorLin <raytaylorlin@gmail.com>
2014-04-25 11:17:09 +0800

    添加快速排序单元测试

---
+++ test/sort/test_quick_sort.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from algorithm.sort.quick_sort import quickSort
+from test_sort import *
+
+
+class TestQuickSort(TestSorting):
+
+    def testBaseCase(self):
+        u"""测试正常情况（随机产生10个数）"""
+        self._testBaseCase(quickSort)
+
+    def testMergeSortCase(self):
+        u"""测试文件样例"""
+        self._testSortingCase(quickSort)
+
+
+if __name__ == '__main__':
+    unittest.main()
