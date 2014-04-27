42646cd273f33b66af6432c62947cd5824f1bcb2
RayTaylorLin <raytaylorlin@gmail.com>
2014-04-27 17:06:06 +0800

    添加归并排序合并两个有序数组的单元测试

--- test/sort/test_merge_sort.py
+++ test/sort/test_merge_sort.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from algorithm.sort.merge_sort import mergeSort
+from algorithm.sort.merge_sort import *
 from test_sort import *
 
 
 class TestMergeSort(TestSorting):
+    TEST_MERGE_FORMAT = '''
+    Input: leftList = %s rightList = %s
+    Result: %s
+    '''
+
+    def testMerge(self):
+        u"""测试合并两个有序数组"""
+
+        self.input = {
+            'leftList': [1, 3, 5],
+            'rightList': [2, 4, 6]
+        }
+        self.correct = [1, 2, 3, 4, 5, 6]
+        self.output = merge(**self.input)
+
+        logger.debug(TestMergeSort.TEST_MERGE_FORMAT, self.input[
+                     'leftList'], self.input['rightList'], self.output)
+        self.assertEqual(self.correct, self.output)
 
     def testBaseCase(self):
         u"""测试正常情况（随机产生10个数）"""
