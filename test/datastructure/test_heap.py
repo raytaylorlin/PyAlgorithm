90d440dd2c14b89f3c1e3e5e81c8ed477d54516c
RayTaylorLin <raytaylorlin@gmail.com>
2014-05-02 16:58:43 +0800

    添加堆的初始化（建立最小堆）测试样例

--- test/datastructure/test_heap.py
+++ test/datastructure/test_heap.py
@@ -25,16 +25,21 @@ class TestHeap(util.PyAlgorithmTestCase):
     def testBubbleDown(self):
         u"""测试堆的向下冒泡调整操作"""
 
-        self.input = [3, 2, 4, 5, 1]
-        heap = Heap(self.input)
+        heap = Heap()
+        heap._heap = [3, 2, 4, 5, 1]
+
         # 调整第2个元素
         heap._bubbleDown(1)
         self.assertEqual([3, 1, 4, 5, 2], heap._heap)
-
         # 调整根元素
         heap._bubbleDown(0)
         self.assertEqual([1, 2, 4, 5, 3], heap._heap)
 
+    def testBuildHeap(self):
+        u"""测试堆的初始化并建立最小堆"""
+
+        heap = Heap([3, 2, 6, 4, 1, 5])
+        self.assertEqual([1, 2, 5, 4, 3, 6], heap._heap)
 
 if __name__ == '__main__':
     unittest.main()
