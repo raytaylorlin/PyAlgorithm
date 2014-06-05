92c46c904e8f0cc7f2406f4a663d2327bf415861
RayTaylorLin <raytaylorlin@gmail.com>
2014-06-05 21:50:31 +0800

    完成Dijkstra算法单元测试样例

---
+++ test/graph/test_dijkstra_shotest_path.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+from algorithm.graph.dijkstra_shotest_path import dijkstraShotestPath
+
+
+class TestDijkstra(unittest.TestCase):
+
+    def testDijkstra(self):
+        graph = {
+            1: [(2, 1), (3, 4)],
+            2: [(3, 2), (4, 6)],
+            3: [(4, 3)]
+        }
+        start = 1
+        self.correct = {
+            1: 0,
+            2: 1,
+            3: 3,
+            4: 6
+        }
+
+        print 'Input: graph = {0}'.format(graph)
+        print 'Input: start vertex = {0}'.format(start)
+        self.output = dijkstraShotestPath(graph, start)
+        print 'Result: ', self.output
+        self.assertEqual(self.correct, self.output)
+
+
+if __name__ == '__main__':
+    unittest.main()
