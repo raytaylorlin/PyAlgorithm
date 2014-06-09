7ddd208f006d31c0856b85cace46c3793c932ea3
RayTaylorLin <raytaylorlin@gmail.com>
2014-06-09 19:47:04 +0800

    添加读取数据文件并标准化的util工具方法

--- test/util.py
+++ test/util.py
@@ -7,14 +7,21 @@ import os
 CASE_FILE_SUFFIX = '.in'
 ANSWER_FILE_SUFFIX = '.ans'
 
-def getCases(directory):
+
+def getCases(directory, algoName):
     fileMap = {}
     caseAnswerList = []
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith(CASE_FILE_SUFFIX):
+            if file.endswith(CASE_FILE_SUFFIX) and file.startswith(algoName):
                 filename = file.replace(CASE_FILE_SUFFIX, '')
                 inputFile = directory + '/' + filename + CASE_FILE_SUFFIX
                 answerFile = directory + '/' + filename + ANSWER_FILE_SUFFIX
                 caseAnswerList.append((inputFile, answerFile))
     return caseAnswerList
+
+
+def readFileAndStandardize(filename, standardizeFunc):
+    with open(filename) as file:
+        data = file.read()
+        return standardizeFunc(data)
